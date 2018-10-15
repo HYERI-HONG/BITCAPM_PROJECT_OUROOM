d0dcc1ebca077543bbf579cbb04abd7aa333abf0
류재경 <jzeror@gmail.com>
2018-10-15 09:44:42 +0900

    10-15-9:44

--- src/main/web/resources/js/jaekyung.js
+++ src/main/web/resources/js/jaekyung.js
@@ -9,8 +9,8 @@ jaekyung =(()=>{
 jaekyung.main={//메인페이지
               mp:()=>{
                      $('#content').empty();                   
-                     $('<div />').attr({id:"ctner__st"}).addClass("container").appendTo('#content').append(
-                    		 $('<div />').attr({id:"ctnerh__st"}),
+                     $('<div />').attr({id:"ctner__st"}).appendTo('#content').append(
+                    		 $('<div />').attr({id:"ctnerh__st" , style:"height:50px"}),
                     		 $('<div />').attr({id:"ctnerb__st"}),
                     		 $('<div />').attr("id","ctnerf__st")
                      );
@@ -20,11 +20,14 @@ jaekyung.main={//메인페이지
               },
               nav:()=>{
                      let navc =$('<nav />');
+                     let d=$('<div />');
                      let u = $('<ul />');
                      let ip = ["통계1","통계2","통계3","통계4","통계5"];
-                     navc.attr({id:"navc__st",style:"position: fixed; max-width:1156px ; padding-right:0px;padding-left:0px ;"})
+                     
+                     navc.attr({id:"navc__st",style:"position: fixed; "})
                      	.addClass("navbar navbar-default navbar-static-top  ");
-                     u.addClass("nav nav-pills nav-justified ").appendTo(navc);
+                     d.appendTo(navc);
+                     u.addClass("nav nav-pills nav-justified ").appendTo(d);
                      $.each(ip,function(){
                            $('<li role="presentation"/>').append($('<a href="#"/>').html(this)).appendTo(u);
                      });
@@ -42,56 +45,62 @@ jaekyung.main={//메인페이지
                      return d;
               },
               smm:()=>{
+            	  let smm = $('<div />');
+            	  let smmb =$('<div />');
             	  let smma = $('<div />');
             	  let smmh = $('<div />');
             	  let smmc = $('<div />');
             	  let smmco = $('<div />');
             	  let smmct = $('<div />');
-            	  smma.addClass("marginstatgap__ryu");
-            	  smmh.html("<h3><u>Summary</u></h3>").appendTo(smma);
-            	  smmc.attr({style:"margin-top:40px"}).addClass("container").appendTo(smma);
+            	  smm.attr({style:"background-color: rgba(204, 82, 0, 0.25);"});
+            	  smmb.appendTo(smm);
+            	  smma.addClass("container compogap__ryu").appendTo(smmb);
+            	  smmh.addClass("txtcw__ryu").html("<h1><strong>요약</strong></h1><hr class='txthr__ryu'/>").appendTo(smma);
+            	  smmc.attr({style:"margin-top:40px;"}).appendTo(smma);
             	  smmco.appendTo(smmc);
             	  smmco.attr({style:"display:flex"}).addClass("row").append(
-            		$('<div />').attr({style:"border:1px solid;"}).addClass("col-sm-6").append(
+            		$('<div />').attr({style:"border:1px solid;"}).addClass("col-sm-6 bgcw__ryu").append(
             			$('<img src="'+$.img()+'/1.PNG" />').addClass("img-responsive center-block")
             		),
             		$('<div />').addClass("col-sm-6").append(
-            				$('<div />').attr({style:"border:1px solid;margin-bottom:15px ; "}).addClass("col-sm-12").append(
+            				$('<div />').attr({style:"border:1px solid;margin-bottom:15px ; "}).addClass("col-sm-12 bgcw__ryu").append(
             						$('<img src="'+$.img()+'/2.PNG" />').attr({style:"margin-bottom:15px"}).addClass("img-responsive center-block")
             				),
-            				$('<div />').attr({style:"border:1px solid;"}).addClass("col-sm-12").append(
+            				$('<div />').attr({style:"border:1px solid;"}).addClass("col-sm-12 bgcw__ryu").append(
             						$('<img src="'+$.img()+'/3.PNG" />').addClass("img-responsive center-block")
             				)
             		)
             	  );
             	  smmct.appendTo(smmc);
             	  smmct.attr({style:"display:flex"}).addClass("row margintop20__ryu").append(
-            		$('<div />').attr({style:"border:1px solid; margin-right:15px"}).addClass("col-sm-6").append(
+            		$('<div />').attr({style:"border:1px solid; margin-right:15px"}).addClass("col-sm-6 bgcw__ryu").append(
             				$('<img src="'+$.img()+'/4.PNG" />').addClass("img-responsive center-block")
             		),
             		$('<div />').addClass("col-sm-3").append(
-            				$('<div />').attr({style:"border:1px solid; "}).addClass("col-sm-12 ").append(
+            				$('<div />').attr({style:"border:1px solid; "}).addClass("col-sm-12 bgcw__ryu").append(
             						$('<img src="'+$.img()+'/5.PNG" />').addClass("img-responsive center-block")
             				)
             		),
             		$('<div />').addClass("col-sm-3").append(
-            				$('<div />').attr({style:"border:1px solid"}).addClass("col-sm-12").append(
+            				$('<div />').attr({style:"border:1px solid"}).addClass("col-sm-12 bgcw__ryu").append(
             						$('<img src="'+$.img()+'/6.PNG" />').addClass("img-responsive center-block")
             				)
             		)
             	  );
-            	  return smma;
+            	  return smm;
               },
               vst:()=>{
-            	  let smma = $('<div />');
-            	  let smmh = $('<div />');
-            	  let smmc = $('<div />');
-            	  let smmco = $('<div />');
-            	  let smmct = $('<div />');
-            	  smma.addClass("marginstatgap__ryu");
+            	  let vst = $('<div />');
+            	  let vsta = $('<div />');
+            	  let vsth = $('<div />');
+            	  let vstc = $('<div />');
+            	  let vstco = $('<div />');
+            	  let vstct = $('<div />');
+            	  vst.attr({style:"background-color: #FFFFF0"});
+            	  vsta.addClass("container compogap__ryu").appendTo(vst);
             	  $('<div />').addClass("row").append(
             			  $('<div />').addClass("col-sm-12").append(
-	            			  $('<div />').addClass("col-sm-2").html("<h3><u>방문자통계</u></h3>"),
+	            			  $('<div />').addClass("col-sm-3 ").html("<h1><strong>방문자통계</strong></h1><hr class='txthr__ryu' style='max-width: 25rem!important; ' />"),
 	            			  $('<div />').addClass("col-sm-2").append(
 	        					  $('<span>').html("상세데이터  &nbsp; &nbsp;&nbsp;"),
 	        					  $('<label />').addClass('switch').append(
@@ -100,22 +109,22 @@ jaekyung.main={//메인페이지
 		    	            	  )
 			    	         )
     	            	 )
-            	  ).appendTo(smma);
-            	  smmc.attr({style:"margin-top:40px"}).addClass("container").appendTo(smma);
-            	  smmco.appendTo(smmc);
-            	  smmco.addClass("row").append(
+            	  ).appendTo(vsta);
+            	  vstc.attr({style:"margin-top:40px"}).appendTo(vsta);
+            	  vstco.appendTo(vstc);
+            	  vstco.addClass("row").append(
             			  $('<div />').attr({style:"border:1px solid"}).addClass("col-sm-12").append(
             					$('<img src="'+$.img()+'/7.PNG" />').addClass("img-responsive center-block")	  
             			  )       			  
             	  );
-            	  smmct.addClass("margintop20__ryu").appendTo(smmc);
+            	  vstct.addClass("margintop20__ryu").appendTo(vstc);
             	  let r ={list:['날짜','총 방문자','회원','비회원','남','여']};
             	  let rr =[{x1:'8월1일',x2:'651234명',x3:'6548명',x4:'321명',x5:'9412명',x6:'321명'},{x1:'8월2일',x2:'651234명',x3:'6548명',x4:'321명',x5:'9412명',x6:'321명'},{x1:'8월3일',x2:'651234명',x3:'6548명',x4:'321명',x5:'9412명',x6:'321명'}];
             	  let t = $('<table />');
             	  let th = $('<thead />');
             	  let tb = $('<tbody />');
             	  let tr = $('<tr />');
-            	  smmct.addClass("row").append(
+            	  vstct.addClass("row").append(
             			  $('<div />').attr({style:"border:1px solid; padding: 20px 40px 20px 40px"}).addClass("col-sm-12").append(
             					  t.addClass("table table-bordered")
             			  )
@@ -139,7 +148,7 @@ jaekyung.main={//메인페이지
             	  });
             	  
             	  
-            	  return smma;
+            	  return vst;
               },
               sba:()=>{
             	  
