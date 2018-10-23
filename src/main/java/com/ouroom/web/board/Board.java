8ab09bca242d7903a259b9558cabbc99c7a26fe9
kimjieun <whatsportskim@gmail.com>
2018-10-23 17:26:46 +0900

    17:26

--- src/main/java/com/ouroom/web/board/Board.java
+++ src/main/java/com/ouroom/web/board/Board.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Component;
 
 import lombok.Data;
 
-@Component @Data @Lazy
+@Component 
+@Data
+@Lazy
 public class Board {
 	private String seq,regi_date,view_cnt,like_cnt,share_cnt,board_seq,item_seq,image;
 }
